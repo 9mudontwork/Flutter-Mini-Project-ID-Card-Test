7b0ad822c89189280790da03ae3edec1d1684727
Muhammad Imoeb <muhammad@diversition.co.th>
2020-09-25 16:26:51 +0700

    ทดสอบ สร้าง widget template

--- id_card_mini_project/lib/main.dart
+++ id_card_mini_project/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'qoute.dart';
 
@@ -17,6 +18,35 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'car', text: 'ข้อความของ แมว'),
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,9 +57,7 @@ class _QuoteListState extends State<QuoteList> {
           backgroundColor: Colors.redAccent,
         ),
         body: Column(
-          children: quotes
-              .map((quote) => Text('${quote.text} - ${quote.author}'))
-              .toList(),
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
         ));
   }
 }
