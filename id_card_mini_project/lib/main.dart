0ffbdad6dc37d11033687b382c8a1357ee078b9f
Muhammad Imoeb <muhammad@diversition.co.th>
2020-09-25 15:40:33 +0700

    🔨 convert to statefull widget

--- id_card_mini_project/lib/main.dart
+++ id_card_mini_project/lib/main.dart
@@ -4,77 +4,81 @@ void main() {
   runApp(MaterialApp(home: MuCard()));
 }
 
-class MuCard extends StatelessWidget {
+class MuCard extends StatefulWidget {
+  @override
+  _MuCardState createState() => _MuCardState();
+}
+
+class _MuCardState extends State<MuCard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-          title: Text('i3acksp4ce Card'),
-          centerTitle: true,
-          backgroundColor: Colors.grey[850],
-          elevation: 0,
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('i3acksp4ce Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpg'),
+                radius: 40,
+              ),
+            ),
+            Divider(height: 90, color: Colors.grey[600]),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'i3acksp4ce',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'CURRENT DEV LEVEL',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'dev@test.com',
+                  style: TextStyle(
+                      color: Colors.grey[400], fontSize: 18, letterSpacing: 1),
+                ),
+              ],
+            ),
+          ],
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/thumb.jpg'),
-                  radius: 40,
-                ),
-              ),
-              Divider(height: 90, color: Colors.grey[600]),
-              Text(
-                'NAME',
-                style: TextStyle(color: Colors.grey, letterSpacing: 2),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'i3acksp4ce',
-                style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30),
-              Text(
-                'CURRENT DEV LEVEL',
-                style: TextStyle(color: Colors.grey, letterSpacing: 2),
-              ),
-              SizedBox(height: 10),
-              Text(
-                '8',
-                style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                children: [
-                  Icon(
-                    Icons.email,
-                    color: Colors.grey[400],
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    'dev@test.com',
-                    style: TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 18,
-                        letterSpacing: 1),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
